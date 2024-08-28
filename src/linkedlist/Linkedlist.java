636d0ee71cb486118cfb29e040c479efc1049c6b
Timona orado <wisem9086@gmail.com>
2024-08-28 23:57:32 +0300

    Linked list java

---
+++ src/linkedlist/Linkedlist.java
@@ -0,0 +1,14 @@
+package linkedlist;
+import java.util.LinkedList;
+public class Linkedlist {
+	public static void main(String[] args) {
+		 LinkedList<String> cars = new LinkedList<String>();
+		    cars.add("Volvo");
+		    cars.add("BMW");
+		    cars.add("Ford");
+		    cars.add("Mazda");
+		    System.out.println(cars);	
+		
+	}
+
+}
